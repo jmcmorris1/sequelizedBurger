689471aa92b989d76c8dee260be20d4fc9b686b3
Jamyl Ali <Jamyl Ali>
2017-05-12 16:08:49 -0400

    the first of many

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burgers_name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (burgers_name) VALUES ('All American Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Burbon Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Veggie Burger');
